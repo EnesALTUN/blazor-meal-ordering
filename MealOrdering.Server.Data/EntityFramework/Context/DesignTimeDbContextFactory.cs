9d9e15b7ef5623b0e563adf0a452cdd7f7ad01b7
Enes ALTUN <ensalt_1998@hotmail.com>
2022-01-17 23:25:35 +0300

    added appsettings connectionstring

--- MealOrdering.Server.Data/EntityFramework/Context/DesignTimeDbContextFactory.cs
+++ MealOrdering.Server.Data/EntityFramework/Context/DesignTimeDbContextFactory.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using System;
 
 namespace MealOrdering.Server.Data.EntityFramework.Context
 {
     public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<MealOrderingDbContext>
     {
+        private readonly IConfiguration _configuration;
+
+        public DesignTimeDbContextFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public MealOrderingDbContext CreateDbContext(string[] args)
         {
-            String connectionString = "Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=f3-EFa1bF";
+            String connectionString = _configuration.GetConnectionString("PostgreSqlConn");
 
             var builder = new DbContextOptionsBuilder<MealOrderingDbContext>();
 
