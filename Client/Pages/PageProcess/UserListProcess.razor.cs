f76ae2369af013afd1cabad6c426f43407205f69
Enes ALTUN <ensalt_1998@hotmail.com>
2022-01-24 20:50:23 +0300

    added user list page

---
+++ Client/Pages/PageProcess/UserListProcess.razor.cs
@@ -0,0 +1,28 @@
+﻿using MealOrdering.Entities.Dto;
+using MealOrdering.Entities.Response;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace MealOrdering.Client.Pages.User
+{
+    public class UserListProcess : ComponentBase
+    {
+        [Inject]
+        public HttpClient Client { get; set; }
+
+        protected List<UserDto> Users = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadList();
+        }
+
+        protected async Task LoadList()
+        {
+            var serviceResponse = await Client.GetFromJsonAsync<ServiceResponse<List<UserDto>>>("api/user");
+
+            if (serviceResponse.Success)
+                Users = serviceResponse.Data;
+        }
+    }
+}
