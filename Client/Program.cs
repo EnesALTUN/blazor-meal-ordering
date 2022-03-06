cf1080934333aedbf8a8b22df00669ddf6465065
Enes ALTUN <ensalt_1998@hotmail.com>
2022-03-06 18:29:10 +0300

    added blazored toastr

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 using Blazored.LocalStorage;
 using Blazored.Modal;
+using Blazored.Toast;
 using MealOrdering.Client;
 using MealOrdering.Client.Utilities;
 using Microsoft.AspNetCore.Components.Web;
@@ -13,6 +14,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddBlazoredModal();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredToast();
 builder.Services.AddScoped<ModalManager>();
 
 await builder.Build().RunAsync();
